f91d53a0370374f4b3d26e8ecba1eda1d4f6ff79
frendygerung22022002 <frendygerung22022002@gmail.com>
2025-07-05 07:23:27 +0000

    refactor: sedikit perbaikan di Logo.tsx

--- src/components/elements/Logo.tsx
+++ src/components/elements/Logo.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
- import Image from 'next/image';
+import Image from 'next/image';
  
  const Logo = () => {
    return (
      <div className="flex items-center gap-3">
        <div className="bg-gray-100 p-1 rounded-md shadow">
-         <Image src="/images/logo.png" alt="Logo STTIS Siau" width={40} height={40} />
+         <Image src="/images/logo.png" 
+         alt="Logo STTIS Siau" 
+         width={160} 
+         height={40} 
+         className='h-10 w-auto rounded-full object-cover'
+         priority/>
        </div>
        <span className="text-2xl font-bold text-gray-800">STTIS Siau</span>
      </div>
