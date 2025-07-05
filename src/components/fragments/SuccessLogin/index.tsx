1a2a037ef72c30a8eb2849872fd928d149c4bd8a
frendygerung22022002 <frendygerung22022002@gmail.com>
2025-07-05 07:27:03 +0000

    style: memperbaiki background transparan pada modal login success saat login

--- src/components/fragments/SuccessLogin/index.tsx
+++ src/components/fragments/SuccessLogin/index.tsx
@@ -40,7 +40,7 @@ const SuccessLogin: React.FC<SuccessLoginProps> = ({ onAnimationComplete }) => {
   return (
     <AnimatePresence>
       <motion.div
-        className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+        className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-black/50"
         initial="hidden"
         animate="visible"
         exit="exit"
