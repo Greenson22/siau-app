151eb32913fea5932e85f5dbbd238178add9bb7b
frendygerung22022002 <frendygerung22022002@gmail.com>
2025-08-20 01:21:09 +0000

    fix: memperbaiki turbo pack yang tidak bisa fast refresh

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    // @ts-ignore - Opsi ini diperlukan untuk HMR di lingkungan cloud
+    allowedDevOrigins: ["*.cloudworkstations.dev"],
+  },
 };
 
-module.exports = {
-  allowedDevOrigins: [
-    'local-origin.dev', 
-    '*.local-origin.dev'
-  ],
-}
 
-export default nextConfig;
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// module.exports = {
+//   allowedDevOrigins: [
+//     'local-origin.dev', 
+//     '*.local-origin.dev',
+//     "*.cloudworkstations.dev"
+//   ],
+// }
+
+// export default nextConfig;
